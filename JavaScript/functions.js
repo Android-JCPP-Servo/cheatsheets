cae2c7cff6730039672a0344caa12fbbea905daa
Android-JCPP-Servo <astewart1138@gmail.com>
2022-10-26 10:15:14 -0500

    Add functions cheatsheet

---
+++ JavaScript/functions.js
@@ -0,0 +1,20 @@
+// Example Variable
+let objName = '';
+
+// Traditional
+function funcName() {
+    objName = 'Function';
+}
+
+// Arrow Notation with Brackets
+const funcName = () => {
+    objName = 'Function';
+}
+
+// Inline Arrow Notation
+const funcName = () => objName = 'Function';
+
+// Function Expression
+const funcName = function() {
+    objName = 'Function';
+}
