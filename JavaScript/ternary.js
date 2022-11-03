597d4f794ff9a29e5fd55328af4f5fcba539aaae
Android-JCPP-Servo <astewart1138@gmail.com>
2022-11-03 09:13:54 -0500

    Add multi-level ternary operator

--- JavaScript/ternary.js
+++ JavaScript/ternary.js
@@ -10,7 +10,14 @@ console.log('Basic implementation');
 
 i == true ? console.log('I\'m TRUE!') : console.log('I\'m FALSE!');
 
+
 // Secondary implementation
 console.log('Secondary implementation');
 
 i == true ? (console.log('I\'m TRUE!'), console.log('I am also TRUE!')) : (console.log('I\'m FALSE!'), console.log('I am also FALSE!'));
+
+
+// Multi-level implementation
+console.log('Multi-level implementation');
+
+i == true ? (console.log('I\'m TRUE!'), console.log('Me too!'), console.log('Me three!')) : (console.log('I\'m FALSE!'), console.log('Me too'), console.log('Me three!'));
