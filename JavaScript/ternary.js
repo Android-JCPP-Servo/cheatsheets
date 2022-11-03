2a2e7b71888a464c8f0b1cbbab2d1f9a9e1e88b1
Android-JCPP-Servo <astewart1138@gmail.com>
2022-11-03 09:13:08 -0500

    Add basic implementation of ternary operator

---
+++ JavaScript/ternary.js
@@ -0,0 +1,11 @@
+/**
+ * THE TERNARY OPERATOR
+ */
+
+// GLOBAL VARIABLE
+let i = true;
+
+// Basic implementation
+console.log('Basic implementation');
+
+i == true ? console.log('I\'m TRUE!') : console.log('I\'m FALSE!');
